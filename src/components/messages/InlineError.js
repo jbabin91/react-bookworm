7e0da61ac234ab0ae0cbb1acfd013feeb171fd5c
Jace Babin <jbabin91@gmail.com>
2017-09-24 00:22:58 -0700

    Login form with client side validation.

---
+++ src/components/messages/InlineError.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const InlineError = ({ text }) => (
+  <span style={{ color: "#ae5856" }}>{text}</span>
+);
+
+InlineError.propTypes = {
+  text: PropTypes.string.isRequired
+};
+
+export default InlineError;
